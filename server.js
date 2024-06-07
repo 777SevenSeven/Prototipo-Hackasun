3f00daa57db80ccf9ce174e5434ec3d5edec6690
Arisley Cardoso da Silva <148290892+Arisleycds@users.noreply.github.com>
2024-06-07 13:45:33 -0300

    update server e html

--- server.js
+++ server.js
@@ -1,16 +1,39 @@
-const express = require('express');
-const app = express();
+// - - - - - - - - - - - - - - - - - - Imports - - - - - - - - - - --
+      const express  = require('express');
+      const mongoose = require('mongoose');
+      const bcrypt   = require('bcrypt');
+      const jwt      = require('jsonwebtoken');
+// - - - - - - - - - - - - - - - - - - Declarações - - - - - - - - --
+      const app = express();
 
-app.use(express.static('./public'));
+      app.use('static', express.static('public'));
 
-app.get('/', (req, res) => {
-      res.send("<h1>Home</h1>")
-})
+      function newPorta(porta, file) {
+            app.get(porta, (req, res) => {
+                  res.sendFile(__dirname + `/public/${file}`);
+            })
+      }
+// - - - - - - - - - - - - - - - - - - Redirecionamento - - - - - - -
+      newPorta('/', '/index.html');
 
-app.get('/Consumo Sustentável', (req, res) => {
-      res.sendFile("PG7.html")
-})
+      newPorta('/aulas', 'aulas.html');
 
-app.listen(3000, () => {
-      console.log("Aberto o server");
-})
+      newPorta('/Consumo%Sustentavel', 'consumo-sustentavel.html');
+
+      newPorta('/contato', 'contato.html');
+     
+      newPorta('/cursos', 'cursos.html');
+
+      newPorta('/comunidade', 'forum.html');
+
+      newPorta('/blog', 'news.html');
+
+      newPorta('/privacidade', 'privacidade.html');
+
+      newPorta('/recompensas', 'recompensas.html');
+
+      newPorta('/sobre', 'sobreNos.html');
+
+      newPorta('/termos', 'termos.html');
+// - - - - - - - - - - - - - - - - - - Abertura do Site - - - - - - -
+      app.listen(3000);
