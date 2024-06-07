a500e80600d12cf4534ebdb6ab041b3615a49080
Arisley Cardoso da Silva <148290892+Arisleycds@users.noreply.github.com>
2024-06-07 16:58:06 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 // - - - - - - - - - - - - - - - - - - - Imports  - - - - - - - - - -
+      require('dotenv').config();
       const express  = require('express');
       const mongoose = require('mongoose');
       const bcrypt   = require('bcrypt');
@@ -8,33 +9,54 @@
 
       app.use('static', express.static('public'));
 
-      function newPorta(porta, file) {
-            app.get(porta, (req, res) => {
-                  res.sendFile(__dirname + `/public/${file}`);
-            })
-      }
 // - - - - - - - - - - - - - - - - - Redirecionamento - - - - - - - -
-      newPorta('/', '/index.html');
+function newPorta(porta, file) {
+      
+}
 
-      newPorta('/aulas', 'aulas.html');
+      app.get('/', (req, res) => {
+            res.sendFile(__dirname + `/public/index.html`);
+      });
 
-      newPorta('/Consumo%Sustentavel', 'consumo-sustentavel.html');
+      app.get('/aulas', (req, res) => {
+            res.sendFile(__dirname + `/public/aulas.html`);
+      });
 
-      newPorta('/contato', 'contato.html');
+      app.get('/Consumo%Sustentavel', (req, res) => {
+            res.sendFile(__dirname + `/public/consumo-sustentavel.html`);
+      });
+
+      app.get('/contato', (req, res) => {
+            res.sendFile(__dirname + `/public/contato.html`);
+      });
+
+      app.get('/cursos', (req, res) => {
+            res.sendFile(__dirname + `/public/cursos.html`);
+      });
      
-      newPorta('/cursos', 'cursos.html');
+      app.get('/comunidade', (req, res) => {
+            res.sendFile(__dirname + `/public/forum.html`);
+      });
 
-      newPorta('/comunidade', 'forum.html');
+      app.get('/blog', (req, res) => {
+            res.sendFile(__dirname + `/public/news.html`);
+      });
 
-      newPorta('/blog', 'news.html');
+      app.get('/privacidade', (req, res) => {
+            res.sendFile(__dirname + `/public/privacidade.html`);
+      });
 
-      newPorta('/privacidade', 'privacidade.html');
+      app.get('/recompensas', (req, res) => {
+            res.sendFile(__dirname + `/public/recompensas.html`);
+      });
 
-      newPorta('/recompensas', 'recompensas.html');
+      app.get('/sobre', (req, res) => {
+            res.sendFile(__dirname + `/public/sobreNos.html`);
+      });
 
-      newPorta('/sobre', 'sobreNos.html');
-
-      newPorta('/termos', 'termos.html');
+      app.get('/termos', (req, res) => {
+            res.sendFile(__dirname + `/public/termos.html`);
+      });
 // - - - - - - - - - - - - - - - - Login in ou Sign up - - - - - -  -
       
 
